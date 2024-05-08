0c380c432a011f8d0c83740b4d673e8d49ac59be
sujonifty <sujonifty@gmail.com>
2024-05-09 05:09:52 +0600

    pagination

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b9hcdyj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,13 +27,17 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async(req, res) => {
         const result = await productCollection.find().toArray();
         res.send(result);
+    })
+    app.get('/productsCount', async(req, res) => {
+        const count = await productCollection.estimatedDocumentCount();
+        res.send({count});
     })
 
     // Send a ping to confirm a successful connection
